d659dfc3d4033546f8dbf3e47bb563a5b37ce90b
nbeaman <nbeaman@fau.edu>
2018-12-27 17:55:41 -0500

    Delete TempSalinConfig.h

--- SensorConfig/TempSalinConfig.h
+++
@@ -1,6 +0,0 @@
-#define config_WIFI_SSID "HOME-55A2"
-#define config_WIFI_PASSWORD "3E7D73F4CED37FAC"
-#define config_LED_NUMBER_OF_PINS 12
-#define config_DOxSensorAddress          97
-#define config_SalinitySensorAddress     100
-#define config_TemperatureSensorAddress  102
