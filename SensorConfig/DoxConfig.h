803887cd84dc036cebfbc0cb69eb4d9f5923843d
nbeaman <nbeaman@fau.edu>
2018-12-27 17:57:15 -0500

    Sensor Config Files

---
+++ SensorConfig/DoxConfig.h
@@ -0,0 +1,6 @@
+#define config_WIFI_SSID ""
+#define config_WIFI_PASSWORD ""
+#define config_LED_NUMBER_OF_PINS 7
+#define config_DOxSensorAddress          97
+#define config_SalinitySensorAddress     100
+#define config_TemperatureSensorAddress  102
