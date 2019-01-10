a46e7f87b49b3839183434a7844de18660287651
nbeaman <nbeaman@fau.edu>
2019-01-10 17:26:51 -0500

    Struct SLOG and TLOG working
    Made struct's so data can be seen inside child program.

--- SensorLog/SensorLog.cpp
+++ SensorLog/SensorLog.cpp
@@ -1,28 +1,67 @@
 #include "SensorLog.h" //include the declaration for this class 
 
+#include <string.h>
 
 
-int 			CurrentIndex;
-int 			MAX_LOG_INDEX;
+int 		sCurrentIndex=0;
+int 		sMAX_LOG_INDEX=450;
+bool 		sTIME_TO_SEND_AND_CLEAR=false;
 
-unsigned long 	_slwhen[450];
-char 			_sltype[450];
-float 			_slvalue[450];
- 
+int 		stCurrentIndex=0;
+int 		stMAX_LOG_INDEX=450;
+bool 		stTIME_TO_SEND_AND_CLEAR=false;	
+
+struct SensorDataLog{
+	unsigned long 	when[450];
+	char 			type[450];
+	float 			val[450];	
+}; 
+
+struct SensorTextLog{
+	unsigned long 	when[450];
+	char 			type[450];
+	char * 			Txt[450][15];	
+};
+
+SensorDataLog SLOG;
+SensorTextLog TLOG;
+
+
 //<<constructor>> setup the LED, make pin 13 an OUTPUT
 SensorLog::SensorLog(){
-	CurrentIndex=0;
-	MAX_LOG_INDEX=450;
-	
+	sCurrentIndex=0;
+	sMAX_LOG_INDEX=450;
+	stCurrentIndex=0;
+	stMAX_LOG_INDEX=450;	
 }
  
 //<<destructor>>
 SensorLog::~SensorLog(){/*nothing to destruct*/}
 
-void SensorLog::slog(unsigned int lwhen, char ltype, float lval){
-      CurrentIndex++;
-	  if( CurrentIndex > MAX_LOG_INDEX ) CurrentIndex=0;
-      _slwhen[CurrentIndex] 	= lwhen;
-      _sltype[CurrentIndex] 	= ltype;
-      _slvalue[CurrentIndex] 	= lval;
+float SensorLog::GetVal(int i){
+	return	SLOG.val[i];
+}
+
+void SensorLog::slog(unsigned int when, char type, float val){
+      sCurrentIndex++;
+	  if( sCurrentIndex > sMAX_LOG_INDEX ) sTIME_TO_SEND_AND_CLEAR=true;
+      SLOG.when[sCurrentIndex] 	= when;
+      SLOG.type[sCurrentIndex] 	= type;
+      SLOG.val[sCurrentIndex] 	= val;
+}
+
+void SensorLog::stlog(unsigned int when, char type, char * Txt[15]){
+      stCurrentIndex++;
+	  if( stCurrentIndex > stMAX_LOG_INDEX ) stTIME_TO_SEND_AND_CLEAR=true;
+      TLOG.when[stCurrentIndex] 	= when;
+      TLOG.type[stCurrentIndex] 	= type;
+      strncpy(*TLOG.Txt[stCurrentIndex],*Txt,15);
+}
+
+void SensorLog::sClear(){
+	sCurrentIndex=0;
+}
+
+void SensorLog::stClear(){
+	stCurrentIndex=0;
 }
--- SensorLog/SensorLog.h
+++ SensorLog/SensorLog.h
@@ -6,13 +6,36 @@ class SensorLog{
   public:
 	SensorLog();
 	~SensorLog(); 
-	
-	int CurrentIndex;
-	int MAX_LOG_INDEX;
 
-    void slog(unsigned int lwhen, char ltype, float lval);
-	
+struct SensorDataLog{
+	unsigned long 	when[450];
+	char 			type[450];
+	float 			val[450];	
+};
+
+struct SensorTextLog{
+	unsigned long 	when[450];
+	char 			type[450];
+	char * 			Txt[450][15];	
+};
+
+	int 	sCurrentIndex;
+	int 	sMAX_LOG_INDEX;
+	bool 	sTIME_TO_SEND_AND_CLEAR=false;	
+
+	int 	stCurrentIndex;
+	int 	stMAX_LOG_INDEX;
+	bool 	stTIME_TO_SEND_AND_CLEAR=false;
+
+	SensorDataLog SLOG;
+	SensorTextLog TLOG;
+
+    void 	slog(unsigned int lwhen, char ltype, float lval);
+	void 	stlog(unsigned int lwhen, char ltype, char * Txt[15]);
+	float 	GetVal(int i);
+	void 	sClear();
+	void 	stClear();
+
 };
 
 #endif
-
