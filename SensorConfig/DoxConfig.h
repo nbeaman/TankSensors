182265ecbdc02ee085c23c4f000accf525096cb0
nbeaman <nbeaman@fau.edu>
2019-01-21 18:02:36 -0500

    added logwebserver and time zone
    #define config_SensorLogPHPwebserver "10.0.0.28"
    #define config_TimeZone -18000
    Next figure out how to use  #define config_LED_NUMBER_OF_PINS 12 instead of having to change it in the library every time

--- SensorConfig/DoxConfig.h
+++ SensorConfig/DoxConfig.h
@@ -1,6 +1,8 @@
-#define config_WIFI_SSID ""
-#define config_WIFI_PASSWORD ""
+#define config_WIFI_SSID "HOME-55A2"
+#define config_WIFI_PASSWORD "3E7D73F4CED37FAC"
 #define config_LED_NUMBER_OF_PINS 7
+#define config_SensorLogPHPwebserver "10.0.0.28"
+#define config_TimeZone -18000
 #define config_DOxSensorAddress          97
 #define config_SalinitySensorAddress     100
 #define config_TemperatureSensorAddress  102
